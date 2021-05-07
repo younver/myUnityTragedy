238e2d36b7875ca81a6316cff4b64b1252608dd5
SSL4SH3R <younver.game@gmail.com>
2021-05-07 19:32:17 +0300

    +added: new rotate script

--- Assets/Scripts/Lessons/RotateObject.cs
+++ Assets/Scripts/Lessons/RotateObject.cs
@@ -4,7 +4,11 @@ namespace Lessons
 {
     public class Test : MonoBehaviour
     {
-        [SerializeField] private float _speed = 1f;
+        [SerializeField] private float _speed = 10f;
 
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _speed*Time.deltaTime);
+        }
     }
 }
